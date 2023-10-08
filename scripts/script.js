f29addf0a631cbe7ba1ca90c6a64046e6d52a576
Diego Antonio Angel Rodriguez <daardibujos@gmail.com>
2023-10-08 16:58:15 -0600

    Add basic structure to the calculator

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+let num1, operator, num2;
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+function divide(n1, n2) {
+  return n1 / n2;
+}
+function mathExpression(n1, op, n2) {
+  return op(n1, n2)
+}
